3ca32869919d561a9d09338f0a4971135145c597
LuizSouzaSP <luiz.hsouza@sptech.school>
2025-09-01 13:49:00 -0300

    erro das metricas arrumados

--- java_paymetrics/src/main/java/PayMetrics/Dados.java
+++ java_paymetrics/src/main/java/PayMetrics/Dados.java
@@ -9,51 +9,45 @@ public class Dados{
         Double[] pacotesRecebidos;
         Double[] pacotesEnviados;
 
-        public Dados(Double[] cpu_percent,Double[] ram_percent, Double[] disco_percent, Double[] pacotesRecebidos, Double[] pacotesEnviados){
-            this.cpu_percent = cpu_percent;
-            this.ram_percent = ram_percent;
-            this.disco_percent = disco_percent;
-        }
-
-        public static void alertaCpu(Dados empresa1){
-            for(int i = 0; i < empresa1.cpu_percent.length; i ++){
-                if(empresa1.cpu_percent[i] >= 50 || empresa1.cpu_percent[i] < 70){
-                    System.out.println("Cuidado com a cpu, ela está quase no limite!");
+        public void alertaCpu(){
+            for(int i = 0; i < cpu_percent.length; i ++){
+                if(cpu_percent[i] >= 90){
+                    System.out.println("CRITICO! A cpu está em um estado critico!");
                 }
-                else if(empresa1.cpu_percent[i] < 90){
+                else if(cpu_percent[i] >= 70 && cpu_percent[i] < 90){
                     System.out.println("ALERTA! A cpu está com um uso acima do esperado!");
                 }
-                else if(empresa1.cpu_percent[i] >= 90){
-                    System.out.println("CRITICO! A cpu está em um estado critico!");
+                else if(cpu_percent[i] >= 50 && cpu_percent[i] < 70){
+                    System.out.println("Cuidado com a cpu, ela está quase no limite!");
                 }
             }
         }
-//        public static void alertaRam(){
-//            for(int i = 0; i < ram_percent.length; i ++){
-//                if(ram_percent[i] >= 50){
-//                    System.out.println("Cuidado com a memoria ram, ela está quase no limite do uso!");
-//                }
-//                else if(ram_percent[i] >= 70){
-//                    System.out.println("ALERTA! A memoria ram está com um uso acima do esperado!");
-//                }
-//                else if(ram_percent[i] >= 90){
-//                    System.out.println("CRITICO! A memoria ram está em um estado critico!");
-//                }
-//            }
-//        }
-//        public static void discoAlerta(){
-//            for(int i = 0; i < disco_percent.length; i ++){
-//                if(disco_percent[i] >= 50){
-//                    System.out.println("Cuidado com o disco, ela está quase no limite do uso!");
-//                }
-//                else if(disco_percent[i] >= 70){
-//                    System.out.println("ALERTA! O disco está com um uso acima do esperado!");
-//                }
-//                else if(disco_percent[i] >= 90){
-//                    System.out.println("CRITICO! O disco está em um estado critico!");
-//                }
-//            }
-//        }
+        public void alertaRam(){
+            for(int i = 0; i < ram_percent.length; i ++){
+                if(ram_percent[i] >= 90){
+                    System.out.println("CRITICO! A memoria ram está em um estado critico!");
+                }
+                else if(ram_percent[i] >= 70 && ram_percent[i] < 90){
+                    System.out.println("ALERTA! A memoria ram está com um uso acima do esperado!");
+                }
+                else if(ram_percent[i] >= 50 && ram_percent[i] < 70){
+                    System.out.println("Cuidado com a memoria ram, ela está quase no limite do uso!");
+                }
+            }
+        }
+        public void discoAlerta(){
+            for(int i = 0; i < disco_percent.length; i ++){
+                if(disco_percent[i] >= 50){
+                    System.out.println("Cuidado com o disco, ela está quase no limite do uso!");
+                }
+                else if(disco_percent[i] >= 70){
+                    System.out.println("ALERTA! O disco está com um uso acima do esperado!");
+                }
+                else if(disco_percent[i] >= 90){
+                    System.out.println("CRITICO! O disco está em um estado critico!");
+                }
+            }
+        }
 //        public static void alertaPacotesRecebidos(){
 //            for(int i = 0; i < pacotesRecebidos.length; i ++){
 //                if(pacotesRecebidos[i] >= 50){
